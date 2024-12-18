d25f06fcf04785dbd2e713b77cb64a8340772bfe
Teertharaj Turkar <teertharajturkar@gmail.com>
2024-12-18 18:07:05 +0530

    project digital timer watch completed

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const countDownElement = document.getElementById("countDown");
+const resetValueElement = document.querySelector(".resetValue");
+let startCount = 0;
+let intervalId;
+
+// Function to start the timer
+const startTimer = () => {
+  intervalId = setInterval(() => {
+    countDownElement.innerText = startCount++;
+  }, 1000);
+};
+
+// Function to stop the timer and show stop time
+const stopTimer = () => {
+  showStopValue();
+  clearInterval(intervalId);
+};
+
+// Function to reset the timer
+const resetTimer = () => {
+  startCount = 0;
+  countDownElement.innerText = startCount;
+  clearInterval(intervalId);
+};
+
+// Function to show stop value
+const showStopValue = () => {
+  const newPara = document.createElement("p");
+  newPara.innerText = `The stop time is ${startCount - 1}`;
+  resetValueElement.append(newPara);
+};
+
+// Function to show stop time
+const clearTimeValue = () => {
+  resetValueElement.innerHTML = "";
+};
+// Event listeners
+document.querySelector(".start_btn").addEventListener("click", startTimer);
+
+document.querySelector(".reset_btn").addEventListener("click", resetTimer);
+
+document.querySelector(".stop_btn").addEventListener("click", stopTimer);
+
+document.querySelector(".time_btn").addEventListener("click", showStopValue);
+
+document.querySelector(".clear_btn").addEventListener("click", clearTimeValue);
